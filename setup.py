c7068d8dde3ecbde94ecd82b02921b83772f3c90
DIMNISSV <dmitrijdenisov359@gmail.com>
2023-06-17 10:30:25 +0600

    requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
+with open('requirements.txt', encoding='utf-8') as f:
+    requires = f.read().strip().splitlines()
+
 setup(
     name='JustAuthPy',
-    version='0.1',
+    version='0.0.1',
     packages=['auth_client'],
     url='https://github.com/DIMNISSV/JustAuthPy',
     license='GNU GPL v3.0',
     author='DIMNISSV',
     author_email='dimnissv@yandex.kz',
-    description=''
+    description='',
+    install_requires=requires
 )
