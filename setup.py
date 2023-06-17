9d5da73cd35001d6ae8e8ea7d908b92b1b161700
DIMNISSV <dmitrijdenisov359@gmail.com>
2023-06-17 10:02:13 +0600

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='JustAuthPy',
+    version='0.1',
+    packages=['auth_client'],
+    url='https://github.com/DIMNISSV/JustAuthPy',
+    license='GNU GPL v3.0',
+    author='DIMNISSV',
+    author_email='dimnissv@yandex.kz',
+    description=''
+)
